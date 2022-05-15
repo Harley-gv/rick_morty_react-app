a5421e6e24b8585125983ae1de7448c628b9503e
Harley-gv <hegv1995@gmail.com>
2022-05-15 17:32:41 -0500

    commit v1 project

---
+++ src/components/Location.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './Location.css'
+import tittle from '../img/rick.svg'
+import bg from '../img/bg.svg'
+import Residents from './Residents';
+
+
+function Location() {
+    const [rick, setRick] = useState({})
+    const [id, setId] = useState('')
+
+    useEffect(() => {
+        const random = Math.floor(Math.random() * 126) + 1
+        axios.get(`https://rickandmortyapi.com/api/location/${random}`).then(res => setRick(res.data))
+    }, [])
+
+
+
+    const search = () => {
+        axios.get(`https://rickandmortyapi.com/api/location/${id}`).then(res => setRick(res.data))
+    }
+    return (
+        <div>
+
+            <img src={bg} alt="bg" className='bg' />
+
+            <div className="header">
+
+                <img src={tittle} alt="" className='img' />
+
+                <input type="text" className="input" onChange={e => setId(e.target.value)}
+                    value={id} placeholder='busqueda por ID'/>
+                <button type="button" className="btn btn-success" onClick={search} id='btn1' >Buscar</button>
+            </div>
+
+
+            <section>
+                <ul className='location-list'>
+                    <li><span>nombre:</span><br />{rick.name}</li>
+                    <li><span>tipo:</span><br />{rick.type}</li>
+                    <li><span>dimensíon:</span><br />{rick.dimension}</li>
+                    <li><span>poblacion:</span><br />{rick.residents?.length}</li>
+                </ul>
+
+            </section>
+
+            <div className="card-info">
+
+                <ul className='card-items'>
+                    {rick.residents?.map(e => (
+                        <Residents url={e} key={e} />
+                    ))}
+                </ul>
+
+            </div>
+
+            <footer className='footer'>
+                <p>hecho por harley guerra, estudiante #academlo</p>
+            </footer>
+
+
+        </div>
+    );
+}
+
+export default Location;
---
+++ src/components/Residents.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './Location.css'
+
+const Residents = ({ url }) => {
+    
+    const [resident, setResident] = useState({})
+    useEffect(() => {
+        axios.get(url).then(res => setResident(res.data))
+    },[url])
+    
+    console.log(resident)
+    return (
+        <div>
+            <li>
+                <div className="card mb-3" id="cards">
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={resident.image} id='img' alt='img'/>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h4>{resident.name}</h4>
+                                <h6>{resident.species} - {resident.status}</h6>
+                                <h5>origen: <br /> <h6>{resident.origin?.name}</h6></h5>
+                                <h5>aparicion en episodios: <br /> <b >{resident.episode?.length}</b></h5>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </li>
+        </div>
+    );
+};
+
+export default Residents;
